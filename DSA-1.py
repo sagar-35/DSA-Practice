bbc8349ffceb5d38c392273f0c127284e3d3fb80
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-31 01:56:58 +0600

    update with prime checker

--- DSA-1.py
+++ DSA-1.py
@@ -1,14 +1,20 @@
-# Factorail Practice
-def factorial(n):
-    if n < 0:
-        return "Negative number is not a factorial."
-    elif n == 1 or n == 0:
-        return 1 
-    else:
-        result = 1
-        for i in range(2, n+1):
-            result *= i 
-        return result
+# Prime Checker
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-num = int(input("Enter a number: "))
-print(f"{num}! = ", factorial(num))
+num = int(input("Eneter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else: 
+    print(f"{num} is not a prime number.")
+    

8bc7869efe405dd0d490c9d235e808f7468cdde8
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-30 23:23:44 +0600

    update with new aritmetic operators

--- DSA-1.py
+++ DSA-1.py
@@ -11,4 +11,16 @@ a = a^b
 print("a: ", a)
 print("b: ", b)
 
+#✅ Method 1: Using Arithmetic Operators
+
+a = 5
+b = 3
+
+a = a + b # a = 5 + 3 = 8
+b = a - b # b = 8 - 3 = 5
+a = a - b # a = 8 - 5 = 3 
+
+print("a: ", a)
+print("b: ", b)
+
 

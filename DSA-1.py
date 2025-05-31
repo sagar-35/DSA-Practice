4f27d175724a24a8a3662f6bfac3eed5f3871991
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-05-31 22:18:40 +0600

    update with vowels finder

--- DSA-1.py
+++ DSA-1.py
@@ -1,20 +1,18 @@
-# Prime Checker
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
+# Problems vowels in a string
+# in: Hello World
+# out: 3
+# START
+# Input user theke
+# count set korbo 0 
+# for each character in string:
+#       if character is vowel(a, e, i, o, u) or uppercase version:
+#       count +1
+# print count
 
-num = int(input("Eneter a number: "))
-if is_prime(num):
-    print(f"{num} is a prime number.")
-else: 
-    print(f"{num} is not a prime number.")
-    
+text = input("Enter any string: ")
+count = 0
+
+for chr in text:
+    if chr.isalpha() and chr.lower() in "aeiou":
+        count += 1
+print("Number of vowel: ", count)

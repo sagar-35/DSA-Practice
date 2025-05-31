46bdef86017f5397f1488da93f4260f90c164034
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-06-01 01:26:01 +0600

    anagrmas checker

--- DSA-1.py
+++ DSA-1.py
@@ -1,9 +1,14 @@
-#Palindrome checker
-text = input("Enter a string: ")
-reverse = ""
-for i in range(len(text)-1, -1, -1):
-    reverse += text[i]
-if text == reverse:
-    print("Yes! It is a palindrome.")
+#Angrams checker
+text1 = input("Enter first string: ")
+text2 = input("Enter second string: ")
+
+text1 = text1.lower().replace(" ", "")
+text2 = text2.lower().replace(" ", "")
+
+sorted_text1 = sorted(text1)
+sorted_text2 = sorted(text2)
+
+if sorted_text1 == sorted_text2:
+    print("They are Anagrams")
 else:
-    print("Sorry! It's not a palindrome")
+    print("Not Anagrams!")

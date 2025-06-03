b663bf96a58de380d10aac97df052a76619b53c2
Ahmedul Haque Sagar <ahmedul35-525@diu.edu.bd>
2025-06-03 22:15:46 +0600

    new Palindrome number practice

--- practice.py
+++ practice.py
@@ -1,23 +1,19 @@
-# factorial finding
-# START
-# user input
-# if user input < 0
-# print negative numbers not for factorials
-# elif user input is equals to 0 and 1 
-# it's factorial will be 1 
-# factorial variables 1 
-# for loop 2 theke n porjontto 1 ghore kore barbe 
-# print 
-# END
-
-num = int(input("Enter a number: "))
-
-if num < 0: 
-    print("Factorial not for negative numbers.")
-elif num == 0 or num == 1:
-    print(f"Factorial of {num} is 1.")
+'''
+START
+Input nibo ekta number
+1. Convert number to string
+2. Reverse the string
+3. Compare original string with reversed string
+    If same:
+        Print "Palindrome"
+    Else:
+        Print "Not Palindrome"
+END
+'''
+num = input("Enter a number: ") #converted num to string
+rev = num[::-1] #reversed stirng
+if num == rev:
+    print(f"{num} is a Palindrome number.")
 else:
-    factorial = 1
-    for i in range(2, num+1):
-        factorial *= i
-    print(f"{num}! = {factorial}")
+    print(f"{num} is not a Palindrome number.")
+
